74d7fa410bd0dfa59f7a81f53f4fcbc0e2f4c918
Adjoa Darien <adjoa.darien@gmail.com>
2018-02-02 15:36:54 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -22,16 +22,16 @@ INSERT INTO users (name, age) VALUES
 
 
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES
-("NANSEN", "Publishing", 19000.00, 2018-01-01, 2018-01-31),
-("Battling in All Her Finery", "Publishing", 5000.00, 2018-02-01, 2018-02-28),
-("A Man Like You EP", "Music", 2000.00, 2018-01-15, 2018-02-15),
-("Queen of the Night", "Music", 5000.00, 2018-03-27, 2018-04-12),
-("Nemesis", "Games", 1200.00, 2018-07-19, 2018-08-05),
-("The Dino Run", "Games", 3000.00, 2018-02-06, 2018-03-04),
-("Blossom Detective Holmes", "Film", 60000.00, 2018-04-09, 2018-07-07),
-("16 Candles", "Film", 80000.00, 2018-09-12, 2018-10-12),
-("Fablewood", "Design & Tech", 12000.00, 2018-01-16, 2018-03-29),
-("Bellcycles", "Design & Tech", 6000.00, 2018-06-16, 2018-06-28);
+("NANSEN", "Publishing", 19000.00, '2018-01-01', '2018-01-31'),
+("Battling in All Her Finery", "Publishing", 5000.00, '2018-02-01', '2018-02-28'),
+("A Man Like You EP", "Music", 2000.00, '2018-01-15', '2018-02-15'),
+("Queen of the Night", "Music", 5000.00, '2018-03-27', '2018-04-12'),
+("Nemesis", "Games", 1200.00, '2018-07-19', '2018-08-05'),
+("The Dino Run", "Games", 3000.00, '2018-02-06', '2018-03-04'),
+("Blossom Detective Holmes", "Film", 60000.00, '2018-04-09', '2018-07-07'),
+("16 Candles", "Film", 80000.00, '2018-09-12', '2018-10-12'),
+("Fablewood", "Design & Tech", 12000.00, '2018-01-16', '2018-03-29'),
+("Bellcycles", "Design & Tech", 6000.00, '2018-06-16', '2018-06-28');
 
 
 INSERT INTO pledges (amount, user_id, project_id) VALUES
